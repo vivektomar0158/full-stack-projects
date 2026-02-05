bf31d666ee7e7497df6212a95c91c51a6f32c12b
Vivek <vivektomar0158@gmail.com>
2026-02-05 10:43:27 +0530

    Add CORS configuration for production

--- src/main/java/com/vivek/expense_tracker/config/SecurityConfig.java
+++ src/main/java/com/vivek/expense_tracker/config/SecurityConfig.java
@@ -46,7 +46,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://localhost:3000",
+                "https://expense-tracker-frontend-l779.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
